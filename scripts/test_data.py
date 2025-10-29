29a5d0c7418259f0be2e57756e46f15ab1fd6c65
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-10-29 10:09:09 -0300

    refactor(test dataload): use get_data instead of manually converting the dataframe

--- scripts/test_data.py
+++ scripts/test_data.py
@@ -1,6 +1,6 @@
 from psycopg import Connection
 from psycopg.rows import TupleRow
-import test_data_generator as test_data
+from test_data_generator import *
 
 
 def load_test_addresses(conn: Connection[TupleRow]):
@@ -8,9 +8,8 @@ def load_test_addresses(conn: Connection[TupleRow]):
         c.executemany(
             '''
             INSERT INTO address (country, state, city, street, number, postal_code, complement)
-            VALUES (%s, %s, %s, %s, %s, %s, %s)
-            ''', test_data.addresses[['country', 'state', 'city', 'street', 'number', 'postal_code', 'complement']]
-            .itertuples(index=False, name=None)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)''',
+            get_data(addresses, ['country', 'state', 'city', 'street', 'number', 'postal_code', 'complement'])
         )
 
     print('Test addresses initialized')
